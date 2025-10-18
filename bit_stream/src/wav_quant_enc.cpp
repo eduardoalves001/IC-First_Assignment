18780fa2576344c21bf29d881fbafba32ae9aae7
Bernardo Marujo <bernardomarujo@ua.pt>
2025-10-18 15:51:52 +0100

    default

--- bit_stream/src/wav_quant_enc.cpp
+++ bit_stream/src/wav_quant_enc.cpp
@@ -1,19 +1,3 @@
-//------------------------------------------------------------------------------
-//
-// Copyright 2025 University of Aveiro, Portugal, All Rights Reserved.
-//
-// These programs are supplied free of charge for research purposes only,
-// and may not be sold or incorporated into any commercial product. There is
-// ABSOLUTELY NO WARRANTY of any sort, nor any undertaking that they are
-// fit for ANY PURPOSE WHATSOEVER. Use them at your own risk. If you do
-// happen to find a bug, or have modifications to suggest, please report
-// the same to Armando J. Pinho, ap@ua.pt. The copyright notice above
-// and this statement of conditions must remain an integral part of each
-// and every copy made of these files.
-//
-// Armando J. Pinho (ap@ua.pt)
-// IEETA / DETI / University of Aveiro
-//
 #include <iostream>
 #include <vector>
 #include <cmath>
